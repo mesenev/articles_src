8defb222f9a78bf891077d53b377bc72fe8667bd
Pavel Mesenev <mesenev.pr@gmail.com>
2018-03-16 02:50:37 +1000

    fix obvious mistake

--- phd_2/experiments/heat_transfer_2d.py
+++ phd_2/experiments/heat_transfer_2d.py
@@ -1,12 +1,15 @@
 from dolfin import *
 from dolfin.cpp.common import set_log_level
+from dolfin.cpp.function import near
 from dolfin.cpp.mesh import UnitSquareMesh, BoundaryMesh
-import logging
+set_log_level(50)
 
 def solve_boundary():
-    set_log_level(50)
     omega = UnitSquareMesh(12, 12)
-    omega_boundary = BoundaryMesh(omega, 'interior')
+    # TODO: https://fenicsproject.org/qa/3074/get-function-values-on-boundaries/
+    # Current 'boundaries' trick is not working :(
+    Right = AutoSubDomain(lambda x, on_bnd: near(x[0], 1) and on_bnd)
+    Top = AutoSubDomain(lambda x, on_bnd: near(x[1], 1) and on_bnd)
     a = 0.006
     alpha = 0.3333
     ka = 1
@@ -17,18 +20,22 @@ def solve_boundary():
     f = Expression('(x[0]+x[1])/2', degree=2)
     finite_element = FiniteElement("CG", omega.ufl_cell(), 2)
     state_space = FunctionSpace(omega, finite_element * finite_element)
+    simple_fs = FunctionSpace(omega, finite_element)
+    boundary_func = Function(simple_fs)
+    DirichletBC(simple_fs, 1, Right).apply(boundary_func.vector())
     state = Function(state_space)
     adjoint = Function(state_space)
     theta, phi = split(state)
     p_1, p_2 = TrialFunctions(state_space)
     v, h = TestFunctions(state_space)
     theta_0 = None
-    u_control = Expression("0.1*x[0]", degree=2)
-    theta_b = interpolate(Expression("sin(x[0])*x[1]/2", degree=2), FunctionSpace(omega, finite_element))
+    u_control = Expression("0.1*x[0]+0.1", degree=2)
+    theta_b = interpolate(Expression("sin(x[0])*x[1]/2", degree=2), simple_fs)
 
     quality_history = []
     def quality():
-        quality_history.append(assemble(0.5 * (theta_k-theta_0)**2 * ds(omega))) #+ epsilon * 0.5 * u_control**2*ds(omega)))
+        quality_history.append(assemble(0.5 * (theta_k-theta_0)**2 * ds(omega)))
+        #+ epsilon * 0.5 * u_control**2*ds(omega)))
         return quality_history[-1]
     def set_and_solve_boundary():
         Boundary_problem = a * inner(grad(theta), grad(v)) * dx + alpha * inner(grad(phi), grad(h)) * dx \
@@ -46,24 +53,29 @@ def solve_boundary():
             + 4 * ka * theta_k**3 * inner(b*p_1 - p_2, v) * dx \
             + f * inner(p_1, v) * ds \
             + ka * inner(p_2 - b * p_1, h) * dx
-        J_theta = theta_k * v * ds
+        J_theta = theta_k* (theta_k - theta_0) * v * ds
         solve(Adjoint_problem == J_theta, adjoint)
         return adjoint.split()
 
 
-    u_control = Expression('0.1*x[0]+0.1', degree=2)
+    u_control = Expression('0.1*x[0]+0.3', degree=2)
 
     theta_k, phi_k = set_and_solve_boundary()
     print(quality())
     p_1_k, p_2_k = set_and_solve_adjoint()
     middle_point_history = []
     for _ in range(98):
-        u_control = Expression('u - lmbd*(p_2 +u*eps)', u=u_control, lmbd=lmbd, p_2=p_2_k, eps=epsilon, degree=2)
+        u_control = Expression('u - lmbd*(p_2+eps*u)', u=u_control, lmbd=lmbd, p_2=p_2_k, eps=epsilon, degree=2)
         theta_k, phi_k = set_and_solve_boundary()
         print('{} iteration, quality functional value: {}'.format(_, quality()))
         p_1_k, p_2_k = set_and_solve_adjoint()
         middle_point_history.append((theta_k(p), phi_k(p), p_1_k(p), p_2_k(p)))
     print('Quality diff: {}'.format(max(quality_history) - min(quality_history)))
+    B = interpolate(u_control, simple_fs)
+    print(B.vector()[simple_fs==1])
+    u_control = Expression('0.1*x[0]+0.1', degree=2)
+    theta_k,phi_k = set_and_solve_boundary()
+    print(quality())
     return
 
 
