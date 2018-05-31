f5daf9971cfa7f6b21e284d82ffcbdf00a89b36e
Pavel Mesenev <mesenev.pr@gmail.com>
2018-05-31 14:31:38 +1000

    Class-based implementation

--- phd_2/experiments/main.py
+++ phd_2/experiments/main.py
@@ -1,11 +1,32 @@
-import heat_transfer_3d
-import heat_transfer_2d
-from sample import sample
-# https://fenicsproject.org/docs/dolfin/2017.2.0/python/
+import shutil
+import time
 
-# biharmonic.solve_boundary()
+from dolfin import Constant
 
-heat_transfer_2d.solve_boundary()
+from phd_2.experiments.SolveDirect import SolveReverse
+from phd_2.experiments.utilities import print_3d_boundaries_single
 
-print('print')
+
+def main():
+    start_time = time.time()
+    # checkers()
+    print('Cleaning up a results folder!')
+    try: shutil.rmtree('results/*')
+    except: pass
+    problem = SolveReverse(theta_b=Constant(0.5))
+    state = problem.solve_boundary_with_deflection()
+    problem.set_theta_0(state[0])
+    print(problem.quality())
+    problem.solve_boundary_with_phi_n_der()
+    print(problem.quality())
+    problem.solve_reverse(tolerance=1e-20, iterations=600000)
+    problem.get_gamma_from_phi_n_derivative()
+    print_3d_boundaries_single(problem.gamma, name='founded_gamma_latest', folder='results')
+    end_time = time.gmtime(time.time() - start_time)
+    print("Job's done! Elapsed time: {}".format(time.strftime("%H:%M:%S", end_time)))
+    return problem
+
+
+if __name__ == "__main__":
+    main()
 
