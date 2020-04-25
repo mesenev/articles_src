b33481222a0afc96dcb51f18bd64c1a80209c02d
mesenev <mesenev.pr@gmail.com>
2020-04-25 20:58:21 +1000

    Corrections # 1

--- phd_2/optimization/solver.py
+++ phd_2/optimization/solver.py
@@ -49,7 +49,7 @@ class SolveOptimization(SolveBoundary):
     p1, p2 = TrialFunctions(DefaultValues3D.state_space)
     conjugate = Function(DefaultValues3D.state_space)
     tau, nu = TestFunctions(DefaultValues3D.state_space)
-    epsilon = 0.1 ** 9
+    epsilon = 0.1 ** 12
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -96,7 +96,6 @@ class SolveOptimization(SolveBoundary):
         if add_to_story:
             self.quality_history.append(quality)
         return quality
-
 
     def _gradient_step(self):
         self.solve_boundary()
