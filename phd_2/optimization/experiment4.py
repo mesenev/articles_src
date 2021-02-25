b9e049c6b5c6b08a38aa7e61082ba60ca9ea1dc3
mesenev <heredemonsnearby@mail.ru>
2021-02-25 23:48:37 +1000

    experiment drawing to b\w

--- phd_2/optimization/experiment4.py
+++ phd_2/optimization/experiment4.py
@@ -1,13 +1,15 @@
 import codecs
-from matplotlib import pyplot as plt, lines
 import json
+
+from dolfin import *
+# noinspection PyUnresolvedReferences
+from dolfin import ds, dx
+from matplotlib import pyplot as plt, lines
+from numpy.ma import arange
 
 from solver2d import SolveOptimization as Problem
 from utilities import clear_dir, print_2d_isolines, print_2d_boundaries, draw_simple_graphic, \
     get_normal_derivative
-from dolfin import ds
-from dolfin import *
-from numpy.ma import arange
 
 set_log_active(False)
 
@@ -67,7 +69,7 @@ print('theta_n', assemble(0.5 * (
 
 figure = plt.figure()
 ax = figure.add_axes([0.1, 0.1, 0.8, 0.8])
-# plt.grid(True)
+plt.grid(True)
 theta_n_original = json.load(open('exp4/theta_n_original', 'r'))
 theta_n_0 = json.load(open('exp4/theta_n_0', 'r'))
 theta_n_100 = json.load(open('exp4/theta_n_100', 'r'))
@@ -75,23 +77,23 @@ ticks_minor = [0, 0.25, 0.5, 0.75, 1]
 ticks = [0.125, 0.375, 0.625, 0.875]
 ax.set_xticks(ticks)
 ax.set_xticks(ticks_minor, minor=True)
-x_labels = ['x=0', 'y=1', 'x=1', 'y=0']
+x_labels = ['$x_1 = 0$', '$x_2 = 1$', '$x_1 = 1$', '$x_2 = 0$']
 ax.set_xticklabels(x_labels)
 step = len(theta_n_original['data']) - 1
 x = arange(0, 1 + 1 / step, 1 / step)
 green_legend = lines.Line2D(
-    [], [], color='red', linestyle='-.', label=r'$\partial_n\hat\theta$ эталонное значение'
+    [], [], color='grey', linestyle='-', label=r'$\partial_n \hat\theta$ заданное значение'
 )
 blue_legend = lines.Line2D(
-    [], [], color='blue', linestyle='-', label=r"$\partial_n\theta$ начальное значение"
+    [], [], color='black', linestyle='--', label=r"$\partial_n \theta$ начальное значение"
 )
 yellow_legend = lines.Line2D(
-    [], [], color='green', linestyle='--', label=r'$\partial_n\theta_\lambda$ приближенное решение'
+    [], [], color='black', linestyle='-.', label=r'$\partial_n \theta_\lambda$ приближенное решение'
 )
 plt.legend(handles=[green_legend, blue_legend, yellow_legend], prop={'size': 10})
-plt.plot(x, theta_n_original['data'], "r-.")
-plt.plot(x, theta_n_100['data'], 'g--')
-plt.plot(x, theta_n_0['data'], 'b-')
+plt.plot(x, theta_n_original['data'], "-", color='grey')
+plt.plot(x, theta_n_100['data'], '-.', color='black')
+plt.plot(x, theta_n_0['data'], '-', color='black')
 
 ax.grid(True, axis='x', which='minor')
 ax.grid(True, axis='y')
@@ -114,23 +116,22 @@ ticks_minor = [0, 0.25, 0.5, 0.75, 1]
 ticks = [0.125, 0.375, 0.625, 0.875]
 ax.set_xticks(ticks)
 ax.set_xticks(ticks_minor, minor=True)
-x_labels = ['x=0', 'y=1', 'x=1', 'y=0']
 ax.set_xticklabels(x_labels)
 step = len(theta_b_original['data']) - 1
 x = arange(0, 1 + 1 / step, 1 / step)
 green_legend = lines.Line2D(
-    [], [], color='red', linestyle='-.', label=r'$\hat\theta|_\Gamma$ эталонное значение'
+    [], [], color='grey', linestyle='-', label=r'$\partial_n \hat\theta$ заданное значение'
 )
 blue_legend = lines.Line2D(
-    [], [], color='blue', linestyle='-', label=r"$\theta|_\Gamma$ начальное значение"
+    [], [], color='black', linestyle='--', label=r"$\partial_n \theta$ начальное значение"
 )
 yellow_legend = lines.Line2D(
-    [], [], color='green', linestyle='--', label=r'$\theta|_\Gamma_\lambda$ приближенное решение'
+    [], [], color='black', linestyle='-.', label=r'$\partial_n \theta_\lambda$ приближенное решение'
 )
 plt.legend(handles=[green_legend, blue_legend, yellow_legend], prop={'size': 10})
-plt.plot(x, theta_b_original['data'], "r-.")
-plt.plot(x, theta_b_100['data'], 'g--')
-plt.plot(x, theta_b_0['data'], 'b-')
+plt.plot(x, theta_b_original['data'], "-", color='grey')
+plt.plot(x, theta_b_100['data'], '-.', color='black')
+plt.plot(x, theta_b_0['data'], '--', color='black')
 
 ax.grid(True, axis='x', which='minor')
 ax.grid(True, axis='y')
