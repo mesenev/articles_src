e29bfa057f0943ed09ca22a179c96b4016913c93
mesenev <heredemonsnearby@mail.ru>
2022-04-28 18:07:27 +1000

    ?

--- build_solution.sh
+++ build_solution.sh
@@ -1,7 +1,9 @@
 docker run --rm \
 --env PYTHONPATH=/source \
--p 0.0.0.0:8888:8888 \
+-p 10.13.13.2:8888:8888 \
 -it \
 -v $(pwd):/home/fenics \
 quay.io/fenicsproject/stable:current \
 'jupyter-notebook --ip=0.0.0.0 --NotebookApp.token=""'
+
+#--network host \     # gethostbyname failed
