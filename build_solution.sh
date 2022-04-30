bd73300a6253760db794a3f65184156fcdc30b02
mesenev <heredemonsnearby@mail.ru>
2022-04-30 19:32:04 +1000

    build solution another questionable update

--- build_solution.sh
+++ build_solution.sh
@@ -1,9 +1,9 @@
 docker run --rm \
 --env PYTHONPATH=/source \
--p 10.13.13.2:8888:8888 \
+-p 127.0.0.1:8888:8888 \
 -it \
 -v $(pwd):/home/fenics \
 quay.io/fenicsproject/stable:current \
-'jupyter-notebook --ip=0.0.0.0 --NotebookApp.token=""'
+'jupyter-notebook --ip=0.0.0.0 --NotebookApp.token="tokentoken"'
 
 #--network host \     # gethostbyname failed
