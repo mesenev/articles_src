acb56f25f2eb81fb01b4849455ca6751dc002254
mesenev <heredemonsnearby@mail.ru>
2022-10-23 18:30:22 +1000

    add missing src for 4/5 articles (untranslated & visually broken)

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v $(pwd)/phd_3/article:/source -w /source aergus/latex latexmk -bibtex -aux-directory=AuxDirectory --output-directory=PDF -pdf article.tex
+docker run --rm -it -v $(pwd):/source -w /source aergus/latex latexmk -bibtex -aux-directory=AuxDirectory --output-directory=PDF -pdf article.tex
