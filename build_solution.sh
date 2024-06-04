b1b67bbda929161144c7761fc775413a5d6c711c
mesenev <mesenev_pr@dvfu.ru>
2024-06-04 12:24:07 +1000

    exp1 add

--- build_solution.sh
+++ build_solution.sh
@@ -1,11 +1,6 @@
 docker run --rm \
---env PYTHONPATH=/source:/source/phd_3/experiments \
---name fenics \
--v $(pwd):/source \
+--env PYTHONPATH=/source \
 --workdir /source/phd_3/experiments \
--p 127.0.0.1:8888:8888 \
--it \
-quay.io/fenicsproject/stable:current \
-'jupyter-notebook --ip=0.0.0.0 --NotebookApp.token="tokentoken" & /bin/bash'
+-it -v $(pwd):/source quay.io/fenicsproject/stable:current \
+"python3 experiment1.py"
 
-#--network host \     # gethostbyname failed
